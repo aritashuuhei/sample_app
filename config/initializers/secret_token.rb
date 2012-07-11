54106d2d61a8908a7dd33bcaec84287bf3e6ec9e
arita shuuhei <shuuhei.arita@gmail.com>
2012-07-11 18:18:10 +0900

    Finish static pages

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = '141cee814913ead98b35ea62573483b593504db435029ae257c49cd8523560615c51c6d311f9bf788cd6283fca12a4964f28ee0be24dc21f7df52caf1204b950'
+SampleApp::Application.config.secret_token = '854c5a1bb3284d9b64f982069990b4fe8a5c56ec92be9dceec257180464c6b81291f3d8262f95d48a8dfc5791b0baee8e04101c505f0e0b09fe9ca0331699308'
